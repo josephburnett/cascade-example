69a61b1c391b51ce5e75f3c949cbc1f5587f4e5e
Joseph Burnett <josephburnett79@gmail.com>
2020-01-21 12:24:12 +0100

    Request generation from service.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-goals = help all a b c ns watch clean
+goals = help all a b c setup bench nobench dash shell logs clean
 .DEFAULT_GOAL : help
 .PHONY : $(goals)
 .ONESHELL : $(goals)
@@ -12,40 +12,75 @@ help :
 all : a b c
 
 a : SERVICE = a
-a : WEIGHT = 20ms
-a : CPU = 20m
-a : DEPS = b,c
+a : WEIGHT = 10ms
+a : GEN = 0
+a : CPU = 100m
+a : DEPS = b
 
 b : SERVICE = b
-b : WEIGHT = 20ms
-b : CPU = 20m
-b : DEPS = 
+b : WEIGHT = 10ms
+b : GEN = 0
+b : CPU = 100m
+b : DEPS = c
 
 c : SERVICE = c
-c : WEIGHT = 20ms
-c : CPU = 20m
+c : WEIGHT = 10ms
+c : GEN = 0
+c : CPU = 100m
 c : DEPS = 
 
-a : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g
-a : ns
+a : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g; s/GEN/$(GEN)/g
+a : setup
 	echo $$PARAMS
 	sed "$$PARAMS" deploy/template.yaml | ko apply -f -
 
-b : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g
-b : ns
+b : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g; s/GEN/$(GEN)/g
+b : setup
 	echo $$PARAMS
 	sed "$$PARAMS" deploy/template.yaml | ko apply -f -
 
-c : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g
-c : ns
+c : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g; s/GEN/$(GEN)/g
+c : setup
 	echo $$PARAMS
 	sed "$$PARAMS" deploy/template.yaml | ko apply -f -
 
-ns :
+bench : setup
+	for i in `seq 1 1`; do kubectl -n cascade-example-bench run ab$$i --image=gcr.io/josephburnett-gke-dev/ab --restart=Always -- -s 5 -t 3600 -n 999999 -c 100 "http://a.cascade-example.svc.cluster.local/" ; done
+
+nobench :
+	kubectl delete namespace cascade-example-bench --ignore-not-found
+
+setup :
 	kubectl create namespace cascade-example --dry-run=true -o yaml | kubectl apply -f -
+	kubectl create namespace cascade-example-bench --dry-run=true -o yaml | kubectl apply -f -
 
-watch :
-	watch 'kubectl get hpa -n cascade-example; echo; kubectl get pods -n cascade-example'
+dash :
+	watch 'kubectl get hpa -n cascade-example; echo; kubectl get pods -n cascade-example; echo; kubectl get pods -n cascade-example-bench'
 
-clean :
+shell :
+	kubectl delete pod shell --ignore-not-found
+	kubectl run --generator=run-pod/v1 -it --rm shell --image=busybox /bin/sh
+
+logs :
+	kubectl -n cascade-example logs -f deployment/a cascade-example --since=10m
+
+clean : nobench
 	kubectl delete namespace cascade-example --ignore-not-found
+
+
+
+
+gen : SERVICE = gen
+gen : WEIGHT = 0
+gen : GEN = 1
+gen : CPU = 20m
+gen : DEPS = a
+
+gen : export PARAMS = s/SERVICE/$(SERVICE)/g; s/WEIGHT/$(WEIGHT)/g; s/DEPS/$(DEPS)/g; s/CPU/$(CPU)/g; s/GEN/$(GEN)/g
+gen : setup
+	echo $$PARAMS
+	sed "$$PARAMS" deploy/template.yaml | ko apply -f -
+
+hey : setup
+	kubectl apply -f deploy/load/load.yaml
+
--- cmd/main.go
+++ cmd/main.go
@@ -16,75 +16,121 @@ import (
 
 var (
 	serviceName  = flag.String("service-name", "default", "The name of this service.")
-	weight       = flag.Duration("request-weight-millicore-duration", 200, "Weight in millicores per time unit of each request.")
+	opWeight     = flag.Duration("op-weight-millicore-duration", 0, "Weight in millicores per time unit of each request.")
+	generateOps  = flag.Int("generate-ops", 0, "Operations per second to generate.")
 	dependencies = flag.String("dependencies", "", "Comma-separated list of downstream service dependencies.")
+
+	reporter *metrics.Reporter
+	limiter  *rate.Limiter
+	services []string
 )
 
-func main() {
+func init() {
 	flag.Parse()
+	reporter = metrics.NewReporter(*serviceName)
+	limiter = rate.NewLimiter(1)
+	services = strings.Split(*dependencies, ",")
+}
 
-	http.HandleFunc("/", newHandler())
+func main() {
+
+	if *generateOps > 0 {
+		log.Printf("Generating ops %v\n", *generateOps)
+		intervalMillis := 1000 / *generateOps
+		ticker := time.NewTicker(time.Duration(intervalMillis) * time.Millisecond)
+		for i := 0; i < 100; i++ {
+			go gen(ticker.C)
+		}
+	}
+
+	http.HandleFunc("/", httpHandler)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func newHandler() func(http.ResponseWriter, *http.Request) {
-	reporter := metrics.NewReporter(*serviceName)
-	limiter := rate.NewLimiter(10)
-	services := strings.Split(*dependencies, ",")
-	return func(w http.ResponseWriter, r *http.Request) {
+func op() (string, int) {
 
-		// Metrics and logs
-		start := time.Now()
-		report := reporter.Success
-		status := http.StatusOK
-		output := "( " + *serviceName + " "
-		defer func() {
-			output += fmt.Sprintf("%v )", status)
-			w.WriteHeader(status)
-			w.Write([]byte(output))
-			report(time.Since(start))
-			log.Println(output)
-		}()
+	// Metrics and logs
+	start := time.Now()
+	report := reporter.Success
+	status := http.StatusOK
+	output := "( " + *serviceName + " "
+	done := func() {
+		output += fmt.Sprintf("%v )", status)
+		report(time.Since(start))
+	}
 
-		// Circuit breaker
-		if ok := limiter.In(); !ok {
-			report = reporter.Overload
-			status = http.StatusServiceUnavailable
-			return
+	// Circuit breaker
+	if ok := limiter.In(); !ok {
+		report = reporter.Overload
+		status = http.StatusServiceUnavailable
+		done()
+		return output, status
+	}
+	defer limiter.Out()
+
+	// Do some work
+	burnCpu(*opWeight)
+	output += fmt.Sprintf("%v ", *opWeight)
+
+	// Call some other services
+	failure := false
+	for _, s := range services {
+		if s == "" {
+			continue
 		}
-		defer limiter.Out()
-
-		// Do some work
-		burnCpu(*weight)
-		output += fmt.Sprintf("%v ", *weight)
-
-		// Call some other services
-		failure := false
-		for _, s := range services {
-			if s == "" {
-				continue
-			}
-			svc := fmt.Sprintf("http://%v.cascade-example.svc.cluster.local", s)
-			resp, err := http.Get(svc)
-			if err != nil {
-				failure = true
-				output += fmt.Sprintf("%q ", err.Error())
-			} else {
-				o, _ := ioutil.ReadAll(resp.Body)
-				resp.Body.Close()
-				output += fmt.Sprintf("%v ", string(o))
-			}
+		svc := fmt.Sprintf("http://%v.cascade-example.svc.cluster.local", s)
+		client := &http.Client{
+			Timeout: time.Second * 10,
 		}
+		resp, err := client.Get(svc)
+		switch {
+		case err != nil:
+			failure = true
+			output += fmt.Sprintf("%q ", err.Error())
+		case resp.StatusCode != http.StatusOK:
+			failure = true
+			output += fmt.Sprintf("(%v %v)", s, resp.StatusCode)
+		default:
+			o, _ := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			output += fmt.Sprintf("%v ", string(o))
+		}
+	}
 
-		if failure {
-			report = reporter.Failure
-			status = http.StatusInternalServerError
+	// Error if any dependencies failed.
+	if failure {
+		report = reporter.Failure
+		status = http.StatusInternalServerError
+	}
+
+	done()
+	return output, status
+}
+
+func gen(ch <-chan time.Time) {
+	for {
+		select {
+		case <-ch:
+			go func() {
+				output, _ := op()
+				log.Println(output)
+			}()
 		}
 	}
 }
 
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	output, status := op()
+	w.WriteHeader(status)
+	w.Write([]byte(output))
+	log.Println(output)
+}
+
 func burnCpu(d time.Duration) {
+	if d == 0 {
+		return
+	}
 	done := make(chan struct{})
 	go func() {
 		for {
